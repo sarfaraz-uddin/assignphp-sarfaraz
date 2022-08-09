6b1ae456ed1e52bc8dba7944978c63e64e71607b
sarfaraz-uddin <88956341+sarfaraz-uddin@users.noreply.github.com>
2022-08-09 20:56:04 +0545

    Initial Commit

---
+++ assign-1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Phpassign-sarfaraz</title>
+</head>
+<body>
+    <h1>My first php assignmnet</h1>
+    <?php
+        echo"my name is sarfaraz uddin";
+    ?>
+</body>
+</html>
